dedaf9845d76c89bb0263ab9648568610d8eece5
cristianevalente <65837430+cristianevalente@users.noreply.github.com>
2023-05-06 15:26:04 -0300

    Add files via upload
    Foram adicionados códigos e exclusão de variáveis.

--- Criando Novas Variáveis.py
+++ Criando Novas Variáveis.py
@@ -1,25 +1,25 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[2]:
 
 
 #Relatório de Análise VI
 
 
-# In[2]:
+# In[3]:
 
 
 ##Criando novas variáveis
 
 
-# In[ ]:
+# In[4]:
 
 
 ##Importando o Pandas
 
 
-# In[3]:
+# In[5]:
 
 
 import pandas as pd
@@ -37,37 +37,37 @@ import pandas as pd
 dados = pd.read_csv('aluguel.csv',sep=';')
 
 
-# In[ ]:
+# In[8]:
 
 
 ##Verificando os dez primeiros registros
 
 
-# In[8]:
+# In[9]:
 
 
 dados.head(10)
 
 
-# In[15]:
+# In[10]:
 
 
 dados['Valor Bruto'] = dados['Valor'] + dados['Condominio'] + dados['IPTU']
 
 
-# In[ ]:
+# In[11]:
 
 
 ##Visualizando a tabela
 
 
-# In[16]:
+# In[12]:
 
 
 dados.head(10)
 
 
-# In[ ]:
+# In[13]:
 
 
 ##Criando outra variável que armazenará o valor por metro quadrado (Valor m2). Coletando valor e dividindo por Área
@@ -79,80 +79,80 @@ dados.head(10)
 dados['Valor m2'] = dados['Valor']/ dados['Area']
 
 
-# In[17]:
+# In[15]:
 
 
 ##Visualizando a tabela
 
 
-# In[18]:
+# In[16]:
 
 
 dados.head(10)
 
 
-# In[19]:
+# In[17]:
 
 
 ##Arredondando os valores
 
 
-# In[21]:
+# In[18]:
 
 
 dados['Valor m2'] = dados['Valor m2'].round(2)
 
 
-# In[22]:
+# In[19]:
 
 
 ##Visualizando a tabela
 
 
-# In[23]:
+# In[20]:
 
 
 dados.head(10)
 
 
-# In[24]:
+# In[21]:
 
 
 ##Criando uma variável para o valor bruto do metro quadrado(Valor Bruto m2)
 
 
-# In[25]:
+# In[22]:
 
 
 dados['Valor Bruto m2'] = (dados['Valor Bruto']/dados['Area']).round(2)
 
 
-# In[26]:
+# In[23]:
 
 
 ##Visualizando a tabela
 
 
-# In[27]:
+# In[24]:
 
 
 dados.head(10)
 
 
-# In[28]:
+# In[25]:
 
 
 ##Criando uma variável que abrigue os tipos de imóvel casa e apartamento.
 ##Para tanto, devemos coletar a variável Tipo, recolher esses marcadores e identifica-los
 
 
-# In[29]:
+# In[26]:
 
 
 casa = ['Casa', 'Casa Condominio', 'Casa de Vila']
 
 
-# In[31]:
+# In[27]:
 
 
 ##Criando a variável Tipo Agregado.
@@ -160,22 +160,130 @@ casa = ['Casa', 'Casa Condominio', 'Casa de Vila']
 ##Passando a variável Tipo, seguido pelo método apply()(este método permite que apliquemos uma função a cada registo do DataFrame)
 
 
-# In[34]:
+# In[28]:
 
 
 dados['Tipo Agregado'] = dados['Tipo'].apply(lambda x:'Casa' if x in casa else 'Apartamento')
 
 
-# In[36]:
+# In[29]:
 
 
 ##Exibindo a tabela
 
 
-# In[37]:
+# In[30]:
 
 
 dados.head(10)
+
+
+# In[31]:
+
+
+##Excluindo variáveis
+
+
+# In[32]:
+
+
+dados_aux = pd.DataFrame(dados[['Tipo Agregado','Valor m2', 'Valor Bruto','Valor Bruto m2']])
+
+
+# In[33]:
+
+
+##Verificando o DataFrame
+
+
+# In[34]:
+
+
+dados_aux.head(10)
+
+
+# In[35]:
+
+
+##Excluindo uma variável
+
+
+# In[36]:
+
+
+del dados_aux['Valor Bruto']
+
+
+# In[37]:
+
+
+##Visualizando a tabela
+
+
+# In[38]:
+
+
+dados_aux.head(10)
+
+
+# In[39]:
+
+
+##Excluindo através do método pop
+
+
+# In[40]:
+
+
+dados_aux.pop('Valor Bruto m2')
+
+
+# In[41]:
+
+
+##Verificando do DataFrame
+
+
+# In[42]:
+
+
+dados_aux.head(10)
+
+
+# In[43]:
+
+
+##Excluindo múltiplas variáveis com o método drop()
+
+
+# In[46]:
+
+
+dados.drop(['Valor Bruto', 'Valor Bruto m2'], axis = 1, inplace = True)
+
+
+# In[47]:
+
+
+##Verificando o DataFrame
+
+
+# In[48]:
+
+
+dados_aux.head(10)
+
+
+# In[49]:
+
+
+##Sobrescrevendo o arquivo do Banco de Dados
+
+
+# In[50]:
+
+
+dados.to_csv('aluguel.csv', sep=';', index = False)
 
 
 # In[ ]:
