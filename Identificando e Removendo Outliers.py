9984e239ad62c48a7a0b482013c306ed8e18b70b
cristianevalente <65837430+cristianevalente@users.noreply.github.com>
2023-06-29 20:50:12 -0300

    Add files via upload

--- Identificando e Removendo Outliers.py
+++ Identificando e Removendo Outliers.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[2]:
 
 
 #Relatório de Análise VIII - Identificando e Removendo Outliers
 
 
-# In[2]:
+# In[3]:
 
 
 ##Importando o pandas, o matplotlib e os dados que serão utilizados
 
 
-# In[3]:
+# In[4]:
 
 
 get_ipython().run_line_magic('matplotlib', 'inline')
@@ -22,74 +22,74 @@ import matplotlib.pyplot as plt
 plt.rc('figure', figsize = (14,6))
 
 
-# In[4]:
+# In[5]:
 
 
 ##Atribuindo o arquivo aluguel à variável dados
 
 
-# In[5]:
+# In[6]:
 
 
 dados = pd.read_csv('aluguel.csv', sep = ';')
 
 
-# In[6]:
+# In[7]:
 
 
 ##Criando o box-plot
 
 
-# In[7]:
+# In[8]:
 
 
 dados.boxplot(['Valor'])
 
 
-# In[8]:
+# In[9]:
 
 
 ##Criando uma visualização clara dos dados e realizando uma seleção no DataFrame. Teremos como resultado apenas os dados errôneos
 
 
-# In[9]:
+# In[10]:
 
 
 dados[dados['Valor']>=500000]
 
 
-# In[10]:
+# In[11]:
 
 
 #Gerando algumas estatísticas sobre esses dados. Criando uma series chamada valor.
 
 
-# In[11]:
+# In[12]:
 
 
 valor = dados['Valor']
 
 
-# In[12]:
+# In[13]:
 
 
 ##Calculando Q1, o primeiro quartil
 
 
-# In[13]:
+# In[14]:
 
 
 q1 = valor.quantile(.25)
 q1
 
 
-# In[14]:
+# In[15]:
 
 
 ##Calculando do Q3; em seguida o IIQ, o intervalo interqualítico. Calculando também o limite inferior e superior.
 
 
-# In[15]:
+# In[16]:
 
 
 q1 = valor.quantile(.25)
@@ -99,99 +99,99 @@ limite_inferior = q1 - 1.5 * IIq
 limite_superior = q3 + 1.5 * IIq
 
 
-# In[16]:
+# In[17]:
 
 
 ##Fazendo a seleção dos dados que estão apenas dentro deste dois limites
 
 
-# In[17]:
+# In[18]:
 
 
 selecao = (valor>=limite_inferior)&(valor<=limite_superior)
 dados_new = dados [selecao]
 
 
-# In[18]:
+# In[19]:
 
 
 ##Gerando novamente o box - plot utilizando a variável dados_new
 
 
-# In[19]:
+# In[20]:
 
 
 dados_new.boxplot(['Valor'])
 
 
-# In[20]:
+# In[21]:
 
 
 ##Gerando Histogramas
 
 
-# In[21]:
+# In[22]:
 
 
 dados.hist(['Valor'])
 dados_new.hist(['Valor'])
 
 
-# In[ ]:
+# In[23]:
 
 
 ##Criando o box-plot
 
 
-# In[22]:
+# In[24]:
 
 
 dados.boxplot(['Valor'], by = ['Tipo'])
 
 
-# In[23]:
+# In[25]:
 
 
 ##Fazendo análise para os grupos selecionados
 
 
-# In[24]:
+# In[26]:
 
 
 grupo_tipo = dados.groupby('Tipo')
 
 
-# In[25]:
+# In[27]:
 
 
 ##Criando grupos baseados na variável Valor por Tipo. Mudando o tipo de DataFrameGroup para Series
 
 
-# In[26]:
+# In[28]:
 
 
 grupo_tipo = dados.groupby('Tipo')['Valor']
 
 
-# In[27]:
+# In[29]:
 
 
 ##Visualizando o conteúdo dos dados
 
 
-# In[28]:
+# In[30]:
 
 
 grupo_tipo.groups
 
 
-# In[29]:
+# In[31]:
 
 
 ##Criando estatísticas
 
 
-# In[30]:
+# In[32]:
 
 
 Q1 = grupo_tipo.quantile(.25)
@@ -201,38 +201,38 @@ limite_inferior = Q1-1.5*IIQ
 limite_superior = Q3+1.5*IIQ
 
 
-# In[31]:
+# In[33]:
 
 
 ##Acessando limite_superior de um tipo de imóvel do tipo apartamento
 
 
-# In[32]:
+# In[34]:
 
 
 limite_superior['Apartamento']
 
 
-# In[33]:
+# In[35]:
 
 
 ##identificando dados discrepantes de acordo com os tipos de apartamento
 
 
-# In[35]:
+# In[36]:
 
 
 for tipo in grupo_tipo.groups.keys():
     print(tipo)
 
 
-# In[36]:
+# In[37]:
 
 
 ##Realizando uma seleção com mais de um tipo de variável(Tipo e Valor)
 
 
-# In[40]:
+# In[38]:
 
 
 for tipo in grupo_tipo.groups.keys():
@@ -242,13 +242,13 @@ for tipo in grupo_tipo.groups.keys():
     dados[selecao]
 
 
-# In[41]:
+# In[39]:
 
 
 ##Criando um DataFrame com essa seleção que criamos
 
 
-# In[44]:
+# In[40]:
 
 
 dados_new = pd.DataFrame()
@@ -260,40 +260,64 @@ for tipo in grupo_tipo.groups.keys():
     dados_new = pd.concat([dados_new, dados_selecao])
 
 
-# In[ ]:
+# In[41]:
 
 
 ##Exibindo o DataFrame
 
 
-# In[45]:
+# In[42]:
 
 
 dados_new
 
 
-# In[ ]:
+# In[43]:
 
 
 ##Gerando um novo box-plot utilizando dados_new
 
 
-# In[49]:
+# In[44]:
 
 
 dados_new.boxplot(['Valor'],by = ['Tipo'])
 
 
-# In[50]:
+# In[45]:
 
 
 ##Salvando os dados para utilizá-los em análises posteriores
 
 
-# In[52]:
+# In[46]:
 
 
 dados_new.to_csv('aluguel_residencial_sem_outliers.csv',sep=';', index = False)
+
+
+# In[52]:
+
+
+dados = pd.read_csv('aluguel_amostra.csv', sep = ';')
+
+
+# In[50]:
+
+
+area = plt.figure()
+g1 = area.add_subplot(1, 2, 1)
+g2 = area.add_subplot(1, 2, 2)
+grupo1 = dados.groupby('Tipo Agregado')['Valor']
+label = grupo1.count().index
+valores = grupo1.count().values
+g1.pie(valores, labels = label, autopct='%1.1f%%')
+g1.set_title('Total de Imóveis por Tipo Agregado')
+grupo2 = dados.groupby('Tipo')['Valor']
+label = grupo2.count().index
+valores = grupo2.count().values
+g2.pie(valores, labels = label, autopct='%1.1f%%', explode = (.1, .1, .1, .1, .1))
+g2.set_title('Total de Imóveis por Tipo')
 
 
 # In[ ]:
